407b949e626e30a3eaba1aafa8ea3a4e3a611c22
Levi Cole <31482349+thelevicole@users.noreply.github.com>
2019-04-22 17:03:52 +0100

    Filthy quick landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,66 @@
-const gulp		= require( 'gulp' );
-const babel		= require( 'gulp-babel' );
-const minify	= require( 'gulp-babel-minify' );
+'use strict';
+
+// Generic
+const gulp       = require( 'gulp' );
+
+// Stylesheets
+const sass         = require( 'gulp-sass' );
+const autoprefixer = require( 'gulp-autoprefixer' );
+
+// Javascripts
+const babel  = require( 'gulp-babel' );
+const minify = require( 'gulp-babel-minify' );
+
+// Browser support
+const browsers = [ 'last 2 version', '> 1%', 'ie 8', 'ie 7' ];
 
 // Paths
-const source	= './src';
-const output	= './dist';
+const paths = {
+	input: {
+		stylesheets: [ './sass/**/*.scss' ],
+		javascripts: [ './js/**/*.js', '!./js/min/**/*' ]
+	},
+	output: {
+		stylesheets: './css',
+		javascripts: './js/min'
+	}
+}
 
-// Build JS
-gulp.task( 'default', function() {
-	return gulp.src( `${source}/breakpoints.js` )
-		.pipe( babel( {
+// Process javascript
+gulp.task('javascripts', function() {
+	return gulp.src( paths.input.javascripts )
+		.pipe( babel({
 			'presets': [
-				[ '@babel/env', {
+				['@babel/env', {
 					'targets': {
-						'browsers': [ 'last 2 version', '> 1%', 'ie 8', 'ie 7' ]
+						'browsers': browsers
 					}
-				} ]
+				}]
 			]
-		} ) )
+		}) )
 		.on( 'error', console.error.bind( console ) )
 		.pipe( minify() )
-		.pipe( gulp.dest( output ) );
-} );
+		.pipe( gulp.dest( paths.output.javascripts ) );
+});
+
+// Process stylesheets
+gulp.task('stylesheets', function() {
+    return gulp.src( paths.input.stylesheets )
+        .pipe( sass({
+            outputStyle: 'compressed'
+        }).on( 'error', sass.logError ) )
+        .pipe( autoprefixer({
+            browsers: browsers
+        }) )
+        .pipe( gulp.dest( paths.output.stylesheets ) );
+});
+
+gulp.task('watch', function() {
+
+	gulp.watch(paths.input.stylesheets, ['stylesheets']);
+	gulp.watch(paths.input.javascripts, ['javascripts']);
+
+});
+
+// Gulp default process
+gulp.task('default', ['stylesheets', 'javascripts']);
